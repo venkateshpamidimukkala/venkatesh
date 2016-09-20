a74913b76b44e2c76fafa59f84429b641eac1f6b
venkateshpamidimukkala <venkateshnaidu421@gmail.com>
2016-09-19 23:40:24 -0700

    Create SumDigit.java

---
+++ SumDigit.java
@@ -0,0 +1,34 @@
+# venkatesh
+public class Solution1 {
+	public int sumDigits(String s) {
+		/*
+		 * For each character in the string, 
+			* check if it is a digit
+				* if so, convert it into an integer
+				*add the integer value to sum
+		*/
+		int sum=0;
+		for(int i=0; i<s.length(); i++) {
+			char c = s.charAt(i);
+			
+			if(Character.isDigit(c)) {
+				String sb = new String();
+				sb = s.substring(i,i+1);
+				sum = sum + Integer.parseInt(sb);
+				
+		
+			}
+		}
+			 System.out.println(sum);
+				
+		
+		return sum; 
+	}
+		public static void main(String args[]){
+			Solution1 s1=new Solution1();
+			s1.sumDigits("aadd4456c");
+
+		 }
+	//method end
+	
+}//class def end
